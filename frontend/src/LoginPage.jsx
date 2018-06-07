07ff98e3f7546655e254c9abe7eb0fc4ee6231c1
haakensonb <untamedhawk44@gmail.com>
2018-06-07 02:48:11 -0400

    barebones functionality added to login/register page

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -2,6 +2,31 @@ import React, {Component} from 'react'
 import NavBar from './NavBar'
 
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(event) {
+    console.log("You are logged in")
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className='container'>
@@ -9,6 +34,19 @@ class LoginPage extends Component {
         <p>
           This is the login page.
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username
+            <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+          </label>
+          <br />
+          <label>
+            Password
+            <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+          </label>
+          <br/>
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     )
   }
--- frontend/src/RegistrationPage.jsx
+++ frontend/src/RegistrationPage.jsx
@@ -2,6 +2,38 @@ import React, {Component} from 'react'
 import NavBar from './NavBar'
 
 class RegistrationPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      passwordConfirm: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    // brackets are for ES6 computed property name
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(event) {
+    if (this.state.password === this.state.passwordConfirm) {
+      console.log("Registered");
+    } else {
+      console.log("Passwords don't match");
+    }
+    console.log(this.state)
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className='container'>
@@ -9,6 +41,25 @@ class RegistrationPage extends Component {
         <p>
           This is the registration page
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username
+            <input type="text" name="username" values={this.state.username} onChange={this.handleChange}/>
+          </label>
+          <br />
+          <label>
+            Password
+            <input type="password" name="password" values={this.state.password} onChange={this.handleChange}/>
+          </label>
+          <br />
+          <label>
+            Confirm Password
+            <input type="password" name="passwordConfirm" values={this.state.passwordConfirm} onChange={this.handleChange}/>
+          </label>
+          <br />
+
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     )
   }
