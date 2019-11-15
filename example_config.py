b6f6ba7f45a735606ef9634f0f13b694cf2ebcb1
haakensonb <untamedhawk44@gmail.com>
2019-11-14 20:07:01 -0500

    added example config file

---
+++ example_config.py
@@ -0,0 +1,9 @@
+# Username and password for mailserver such as mailtrap
+MT_USERNAME = ''
+MT_PASSWORD = ''
+
+# use os.urandom to generate a secret key
+SECRET_KEY = b''
+
+# change this to whatever URL React starts on
+FRONTEND_URL_BASE = 'http://localhost:3000'
