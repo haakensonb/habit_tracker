15a37dfed145fb58b3fb906279865e006737dfdc
haakensonb <untamedhawk44@gmail.com>
2019-11-15 20:22:55 -0500

    updated config

--- example_config.py
+++ example_config.py
@@ -1,9 +1,13 @@
 # Username and password for mailserver such as mailtrap
-MT_USERNAME = ''
-MT_PASSWORD = ''
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_SERVER = 'smtp.mailtrap.io'
+MAIL_PORT = 2525
+MAIL_DEBUG = True
 
 # use os.urandom to generate a secret key
-SECRET_KEY = b''
+SECRET_KEY = 'changeme'
+JWT_SECRET_KEY = 'changeme'
 
 # change this to whatever URL React starts on
 FRONTEND_URL_BASE = 'http://localhost:3000'
--- habit_tracker/__init__.py
+++ habit_tracker/__init__.py
@@ -1,18 +1,16 @@
-# sample config, mostly the same as in flask documentation
 import os
-
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import jwt_required, JWTManager
 from habit_tracker.models import RevokedToken
 
-from config import MT_USERNAME, MT_PASSWORD
+from config import MAIL_SERVER, MAIL_PORT, MAIL_DEBUG, MAIL_USERNAME, MAIL_PASSWORD, SECRET_KEY, JWT_SECRET_KEY, FRONTEND_URL_BASE
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=SECRET_KEY,
         DATABASE=os.path.join(app.instance_path, 'habit_tracker.sqlite')
     )
     # Works with three slashes after sqlite, even though documentation had four
@@ -28,17 +26,17 @@ def create_app(test_config=None):
     # setup hashing
     bcrypt.init_app(app)
     # setup flask mail
-    app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-    app.config['MAIL_DEBUG'] = True
+    app.config['MAIL_SERVER'] = MAIL_SERVER
+    app.config['MAIL_DEBUG'] = MAIL_DEBUG
     app.config['MAIL_SUPPRESS_SEND'] = False
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = MT_USERNAME
-    app.config['MAIL_PASSWORD'] = MT_PASSWORD
+    app.config['MAIL_PORT'] = MAIL_PORT
+    app.config['MAIL_USERNAME'] = MAIL_USERNAME
+    app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
 
     from habit_tracker.auth import mail
     mail.init_app(app)
 
-    app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+    app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
     app.config['JWT_BLACKLIST_ENABLED'] = True
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
@@ -50,7 +48,7 @@ def create_app(test_config=None):
 
 
     # setup CORS for react
-    cors = CORS(app, origins=['http://localhost:3000'])
+    cors = CORS(app, origins=[FRONTEND_URL_BASE])
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -67,10 +65,10 @@ def create_app(test_config=None):
         pass
     
     # a simple test page that says hello
-    @app.route('/hello')
+    # @app.route('/hello')
     # @jwt_required
-    def hello():
-        return 'Hello World!'
+    # def hello():
+        # return 'Hello World!'
 
 
     # register the api blueprint with the app
